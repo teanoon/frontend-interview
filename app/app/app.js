ee67c0e94719544a5faa7e7df56622acc884ad30
teanoon <anticipationvu@gmail.com>
2017-03-08 03:29:43 +0000

    add js/css scaffold

---
+++ app/app/app.js
@@ -0,0 +1,5 @@
+import _ from 'lodash'
+import moment from 'moment'
+import index from './javascripts/index.js'
+
+import './app.sass'
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,48 @@
-var path = require('path')
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/app.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [['es2015', {modules: false}]],
+          plugins: [
+            'syntax-dynamic-import',
+            'transform-async-to-generator',
+            'transform-regenerator',
+            'transform-runtime'
+          ]
+        }
+      }]
+    },
+    {
+      test: /\.sass$/,
+      use: ExtractTextPlugin.extract({
+        use: ['css-loader', 'sass-loader']
+      })
+    }]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // Specify the common bundle's name.
+      name: 'vendor',
+      minChunks: (module) => {
+        // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles.css'
+    })
+  ]
 }
